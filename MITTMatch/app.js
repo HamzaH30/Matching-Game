cbecc686bad89c06986e8607f88d78087803b0e3
Hamza Haque <hamzahaque30@gmail.com>
2023-01-17 15:11:58 -0600

    Working without changing score and next card

--- MITTMatch/app.js
+++ MITTMatch/app.js
@@ -30,7 +30,15 @@ function generateGameState() {
   renderBoard();
 }
 
+// This function is responsible for rendering the game board to match the information in the gameState
 function renderBoard() {
+  // // Updating the "Next Card"
+  // const currentNextCard =
+  //   document.getElementById("next-card").firstElementChild.classList[1];
+  // document
+  //   .getElementById("next-card")
+  //   .firstElementChild.classList.replace(currentNextCard, gameState.cardToFind);
+
   const elementCards = [...document.getElementsByClassName("card")];
   for (let i = 0; i < elementCards.length; i++) {
     // Replacing the icon class name to match the icon class name in gameState
@@ -55,9 +63,61 @@ function renderBoard() {
   }
 }
 
+function revealCard(event) {
+  let clickedCardClass;
+  /**
+   * There are 2 areas a user can click
+   * 1. The middle of the card -> the icon element
+   * 2. Somewhere else on the card -> the list element
+   */
+  // Clicked somewhere other than the middle of the card
+  if (event.target.classList.contains("card")) {
+    clickedCardClass = event.target.firstElementChild.classList[1];
+  } else {
+    // Clicked right on the icon
+    clickedCardClass = event.target.classList[1];
+  }
+
+  const gameStateCard = gameState.board.find(
+    (element) => element.icon === clickedCardClass
+  );
+
+  if (clickedCardClass === gameState.cardToFind) {
+    // The card clicked is a match, set matched to true
+    gameStateCard.matched = true;
+    renderBoard();
+
+    // Change the "next card"/card to find
+    // newCardToFind();
+  } else {
+    // The card clicked is not a match
+    // Show the card
+    gameStateCard.show = true;
+    renderBoard();
+
+    // Hide the card again after a set duration
+    gameStateCard.show = false;
+    setTimeout(() => renderBoard(), 375);
+  }
+}
+
+function newCardToFind() {
+  /**
+   * Create a random array of the cards
+   * Loop through all the cards in that array
+   * If the card is not currently matched, then make that the cardToFind
+   */
+  for (let card of shuffle(gameState.board)) {
+    if (!card.matched) {
+      gameState.cardToFind = card.icon;
+    }
+  }
+
+  renderBoard();
+}
+
 /**
- * 1.
- * 2.
+ * 5. If it is a match, then change the cardToFind variable to a new one
  */
 
 const gameState = {
@@ -132,3 +192,4 @@ generateGameState();
 document
   .getElementsByClassName("restart")[0]
   .addEventListener("click", generateGameState);
+document.getElementById("cards").addEventListener("click", revealCard);
